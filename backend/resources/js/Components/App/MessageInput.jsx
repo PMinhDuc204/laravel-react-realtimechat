6f1ce63ced332e52607b177bd660a09ee759afb6
PMinhDuc204 <baorei2010@gmail.com>
2026-03-10 02:40:38 +0700

    feat: implement message input with attachment support, including audio recording and emoji picker.

--- backend/resources/js/Components/App/MessageInput.jsx
+++ backend/resources/js/Components/App/MessageInput.jsx
@@ -7,6 +7,7 @@ import { Popover } from '@headlessui/react';
 import { isAudio, isImage } from '@/helpers';
 import CustomAudioPlayer from './CustomAudioPlayer';
 import AttachmentPreview from './AttachmentPreview';
+import AudioRecorder from './AudioRecorder';
 
 const MessageInput = ({ conversation = null }) => {
     const [newMessage, setNewMessage] = useState("");
@@ -92,7 +93,11 @@ const MessageInput = ({ conversation = null }) => {
             data["group_id"] = conversation.id;
         }
         axios.post(route("message.store"), data);    
-    }
+    };
+
+    const recordedAudioReady = (file, url) => {
+        setChosenFiles((prevFiles) => [...prevFiles, {file, url}]);
+    };
     
   return (
     <div className="flex flex-wrap items-start border-t border-slate-700 py-3">
@@ -116,6 +121,7 @@ const MessageInput = ({ conversation = null }) => {
                     className='absolute left-0 top-0 right-0 bottom-0 z-20 opacity-0 cursor-pointer'
                 />
             </button>
+            <AudioRecorder fileReady={recordedAudioReady} />
         </div>
         <div className="order-1 px-3 xs:p-0 min-w-[220px] basis-full xs:basis-0 xs:order-2 flex-1 relative">
             <div className="flex">
